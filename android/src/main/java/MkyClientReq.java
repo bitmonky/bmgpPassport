6af52d8b1162799f6c8a994ba0708631150d9b81
bitmonky <81542700+bitmonky@users.noreply.github.com>
2023-03-10 10:39:34 -0500

    Add files via upload

---
+++ android/src/main/java/MkyClientReq.java
@@ -0,0 +1,29 @@
+import org.json.*;
+public class MkyClientReq {
+    public String req = "Not Set";
+    public String parms = "Not Set";
+    public String endPoint = "/whzon/gold/netWalletAPI.php";
+    public String host     = "www.bitmonky.com";
+    public String port     = "";
+    public String service  = "https://"+host+port+endPoint;
+    public String publicKey;
+    public String privateKey;
+    public String walletCipher;
+    public String doParse(String j){
+      try {
+        JSONObject obj = new JSONObject(j);
+        req = obj.getString("req");
+        try {parms  = obj.getJSONObject("parms").toString();}
+        catch(Exception e) {}
+        try {
+            endPoint = obj.getJSONObject("service").getString("endPoint");
+            host     = obj.getJSONObject("service").getString("host");
+            port     = obj.getJSONObject("service").getString("port");
+            service  = "https://"+host+port+endPoint;
+        }
+        catch(Exception e) {}
+      }
+      catch(Exception e) {return e.toString() + j;}
+      return "OK";
+    }
+}
--- android/src/main/java/MkyWallet.java
+++ android/src/main/java/MkyWallet.java
@@ -1,14 +1,14 @@
 import org.json.*;
 public class MkyWallet {
 
-    public String ownerMUID = "Not Set";
+    public String ownMUID = "Not Set";
     public String publicKey;
     public String privateKey;
     public String walletCipher;
     public String doParse(String j){
       try {
           JSONObject obj = new JSONObject(j);
-          ownerMUID  = obj.getString("ownMUID");
+          ownMUID  = obj.getString("ownMUID");
           publicKey  = obj.getString("publicKey");
           privateKey = obj.getString("privateKey");
           walletCipher = obj.getString("walletCipher");
