628be9db3f98200fbeffead77a03bc7337bdd959
bitmonky <81542700+bitmonky@users.noreply.github.com>
2023-03-14 00:12:33 -0400

    Add files via upload

--- android/src/main/java/MkyWallet.java
+++ android/src/main/java/MkyWallet.java
@@ -1,10 +1,14 @@
 import org.json.*;
 public class MkyWallet {
-
+    public Integer sPort = 8080;
     public String ownMUID = "Not Set";
     public String publicKey;
     public String privateKey;
     public String walletCipher;
+    public String setPort(Integer inPort){
+        sPort=inPort;
+        return sPort.toString();
+    }
     public String doParse(String j){
       try {
           JSONObject obj = new JSONObject(j);
