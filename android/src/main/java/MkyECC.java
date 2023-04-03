b529e4df758a3922e1b9003ca1680280ccf560bf
bitmonky <81542700+bitmonky@users.noreply.github.com>
2023-04-03 12:18:33 -0400

    Add AES  encryption for mailTree functionality

--- android/src/main/java/MkyECC.java
+++ android/src/main/java/MkyECC.java
@@ -4,9 +4,16 @@ import org.bitcoinj.core.LegacyAddress;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.params.MainNetParams;
+
+import java.io.UnsupportedEncodingException;
 import java.security.*;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import android.util.Base64; //java.util.Base64;
 import java.math.BigInteger;
+import java.security.spec.AlgorithmParameterSpec;
 
 public class MkyECC {
     KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
@@ -14,11 +21,54 @@ public class MkyECC {
     class MkyRSA  {
         String pubKey = "";
         String privKey = "";
+        String esaKey = "";
+        String esaIV  = "";
         public String  toJSON(){
             return "\"rsaKeys\": { " +
               "\"pubKey\":\"" + pubKey +
               "\",\"privKey\":\"" + privKey + "\"}";
         }
+        public String encrypt(String src,String key,String iv) {
+            esaKey = key;
+            esaIV  = iv;
+            try {
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, makeKey(), makeIv());
+                return android.util.Base64.encodeToString(cipher.doFinal(src.getBytes()),android.util.Base64.DEFAULT);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        public String decrypt(String src) {
+            String decrypted = "";
+            try {
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.DECRYPT_MODE, makeKey(), makeIv());
+                decrypted = new String(cipher.doFinal(android.util.Base64.decode(src,android.util.Base64.DEFAULT)));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return decrypted;
+        }
+        private  AlgorithmParameterSpec makeIv() {
+            try {
+                return new IvParameterSpec(esaIV.getBytes("UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        private  Key makeKey() {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                byte[] key = md.digest(esaKey.getBytes("UTF-8"));
+                return new SecretKeySpec(key, "aes-256-cbc");
+            } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
     }
     public MkyECC() throws NoSuchAlgorithmException {
     }
